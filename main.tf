8449cffb20cf26e6236783d95df6398c3a32644b
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-10-12 00:51:11 -0400

    Create VPC

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "main" {
+  cidr_block                       = var.vpc_cidr_block
+  enable_dns_hostnames             = true
+  enable_dns_support               = true
+  enable_classiclink_dns_support   = true
+  assign_generated_ipv6_cidr_block = false
+  tags = {
+    "Name" = var.vpc_name
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,11 @@
 variable "profile" {}
 variable "region" {}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR for VPC"
+}
+
+variable "vpc_name" {
+  default = "csye6225-VPC"
+}
